1d5aec3af546ba5d35f91bb8d6f2aa296d3d1504
Ashish Kumar Saurav <sauravashish.0000@example.com>
2026-06-26 09:17:53 +0530

    Api Call update

--- frontend/lib/formApi.ts
+++ frontend/lib/formApi.ts
@@ -3,11 +3,11 @@ const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 export async function getForm(slug: string) {
   const res = await fetch(
     `${STRAPI_URL}/api/forms?filters[slug][$eq]=${slug}&populate=*`,
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
+    
+       {
+      cache: "no-store",
+    },
+
   );
 
   if (!res.ok) {
